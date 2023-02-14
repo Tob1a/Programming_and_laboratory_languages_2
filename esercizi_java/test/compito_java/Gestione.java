7116b211874dbda70442509bc2e7c5c0cb68a6b5
Tobia Sacchetto <98024054+Tob1a@users.noreply.github.com>
2023-02-14 13:12:44 +0100

    adding final input try catch

--- esercizi_java/test/compito_java/Gestione.java
+++ esercizi_java/test/compito_java/Gestione.java
@@ -51,4 +51,34 @@ public static Void Main(String[] args) {
         System.err.println(e);
     }
     //punto 2
+    try{
+        BufferedReader br = new BufferedReader(new FileReader("clienti.txt"));
+        String line = br.readLine();
+        line = br.readLine();
+        while(line!=null){
+            tok= new StringTokenizer(line);
+            String nome = tok.nextToken();
+            String cognome = tok.nextToken();
+            Cliente c = new Cliente(nome, cognome);
+            clienti.add(c);
+            line =br.readLine();
+            while(line!=null)
+            {
+                tok = new StringTokenizer(line);
+                int cod = Integer.parseInt(tok.nextToken());
+                int giorni = Integer.parseInt(tok.nextToken());
+                c.addProg(codProg.get(cod),giorni);
+                line = br.readLine();
+            }
+            line = br.readLine();
+        }
+        br.close();
+    }catch (Ioexpection e){
+        System.err.println(e);
+    }catch (Exception e){
+        System.err.println(e);
+    }
+    //punto 3
+    
+    
 }
