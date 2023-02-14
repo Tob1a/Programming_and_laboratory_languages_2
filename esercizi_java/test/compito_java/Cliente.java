a0c7bd042846636f38bb3a53c65f20f932a8ded0
Tobia Sacchetto <98024054+Tob1a@users.noreply.github.com>
2023-02-14 10:16:58 +0100

    modified cliente.java

--- esercizi_java/test/compito_java/Cliente.java
+++ esercizi_java/test/compito_java/Cliente.java
@@ -1,4 +1,39 @@
 public class Cliente{
     private String nome,cognome;
     private List<Noleggio> veicolo;
+
+    public class Cliente(String nome, String cognome){
+            this.nome = nome;
+            this.cognome = cognome;
+            this.progetti= new LinkedListl<Noleggio>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome{
+        return cognome;
+    }
+/*
+    public double getMax{
+        double max = 0;
+        Noleggio maxi=null;
+        for(Noleggio i:veicolo){
+            if(i.getPrezzo()>max){
+                max = i.getPrezzo();
+                maxi = i;   
+            }     
+        }
+        return maxi;
+    }
+*/
+    public double getTotal{
+        double total = 0;
+        for(Noleggio i:veicolo){
+            total = i.getPrezzo()+total;
+        }
+        return total;
+    }
+
 }
